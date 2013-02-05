a62df21248bdbc0abd3affe79a97807828aa9d10
HajerSG <hajer.bazaa@ingesup.com>
2013-02-05 12:40:06 +0100

    Initial Commit

---
+++ test/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  test
+//
+//  Created by hajer on 05/02/13.
+//  Copyright (c) 2013 hajer. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
